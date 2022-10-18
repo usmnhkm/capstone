d6c9787db01e81f2847a14e4e51e476e4fb98b9f
usmnhkm <hakimusmanto8@gmail.com>
2022-10-18 09:57:12 +0700

    perbaikan code

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import pandas as pd
-import streamlit as st
+# import pandas as pd
+# import streamlit as st
 
-st.title("Semakin tinggi tingkat pendidikan, Semakin tinggi pula upahnya?")
+# st.title("Semakin tinggi tingkat pendidikan, Semakin tinggi pula upahnya?")
 
-st.write('Data Tingkat Penyelesaian Pendidikan')
-data_pendidikan = pd.read_excel('data/source/Tingkat Penyelesaian Pendidikan Menurut Jenjang Pendidikan dan Provinsi.xlsx')
-st.dataframe(data_pendidikan)
+# st.write('Data Tingkat Penyelesaian Pendidikan')
+# data_pendidikan = pd.read_excel('data/source/Tingkat Penyelesaian Pendidikan Menurut Jenjang Pendidikan dan Provinsi.xlsx')
+# st.dataframe(data_pendidikan)
 
-st.write('Upah Rata-Rata Per jam')
-data_pendidikan = pd.read_excel('data/source/Upah Rata - Rata Per Jam Pekerja Menurut Provinsi.xlsx')
-st.dataframe(data_pendidikan)
+# st.write('Upah Rata-Rata Per jam')
+# data_pendidikan = pd.read_excel('data/source/Upah Rata - Rata Per Jam Pekerja Menurut Provinsi.xlsx')
+# st.dataframe(data_pendidikan)
